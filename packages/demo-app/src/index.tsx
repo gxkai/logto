87cbebb65d7b345b5c0dda6c407b915f9585004d
guxukai <gxk@33.cn>
2023-03-09 15:54:41 +0800

    complete

--- packages/demo-app/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import { createRoot } from 'react-dom/client';
-// eslint-disable-next-line import/no-unassigned-import
-import '@logto/core-kit/declaration';
-
-import App from './App';
-
-const app = document.querySelector('#app');
-const root = app && createRoot(app);
-root?.render(<App />);
